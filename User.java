7d78047d04ff52cf2c38e1c9e3d4159e6f929bce
Shira Goldstein <shira.goldstein@post.runi.ac.il>
2026-01-08 12:15:32 +0200

    Completed assignment

--- User.java
+++ User.java
@@ -43,11 +43,12 @@
 
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
-        for (int i = 0; i < follows.length; i++) {
-            if(follows[i].equals(name))
-                return true;
+       for (int i = 0; i < fCount; i++) {
+        if (follows[i].equalsIgnoreCase(name)) { 
+            return true;
         }
-        return false;
+    }
+    return false;
     }
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
